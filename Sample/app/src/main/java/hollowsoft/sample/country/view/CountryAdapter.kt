ed31d2407a77939207780803c761bffadf03fd16
Igor Morais <igor.morais@hollowsoft.com>
2017-09-27 00:37:18 -0300

    Update sample

--- Sample/app/src/main/java/hollowsoft/sample/country/view/CountryAdapter.kt
+++ Sample/app/src/main/java/hollowsoft/sample/country/view/CountryAdapter.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Igor Morais
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package hollowsoft.sample.country.view
 
 import android.support.v7.widget.RecyclerView
@@ -17,15 +33,15 @@ class CountryAdapter(private val listener: ItemListener<Country>, private val li
 
     override fun getItemCount() : Int = list.size
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup?, type: Int) : ViewHolder {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, type: Int) : ViewHolder {
 
-        val view = LayoutInflater.from(viewGroup?.context).inflate(R.layout.item, viewGroup, false)
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.item, viewGroup, false)
 
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        holder?.bind(list[position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(list[position])
     }
 
     inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
@@ -33,18 +49,11 @@ class CountryAdapter(private val listener: ItemListener<Country>, private val li
         private val name = view.findViewById<TextView>(R.id.name)
         private val image = view.findViewById<ImageView>(R.id.image)
 
-        init {
-
-            view.setOnClickListener {
-
-            }
-        }
-
         fun bind(country: Country) {
 
             name.text = country.name
 
-            country.image?.let {
+            country.image.let {
 
                 image.setImageResource(it)
             }
